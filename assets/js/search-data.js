59b780757b75fda22b5bcd2e4c92623295be4f5d
Asagodi <abel.sagodi@gmail.com>
2025-12-05 18:30:53 +0000

    Deploying to gh-pages from @ Asagodi/asagodi.github.io@3796266081b38bb6cc1174f59d517e1838ed58fa 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-about",
-    title: "about",
+    id: "nav-phd-student",
+    title: "PhD student",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
